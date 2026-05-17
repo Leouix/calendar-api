6e1ac320c2bed0ff3f15a54aaa5ddd1b761de00f
leouix <nsht22sola@gmail.com>
2026-05-17 23:44:54 +0700

    Refactor data fetch logic and update configurations.
    Introduced year-based range fetching for events in the Finnhub provider, replacing hardcoded date calculations. Commented out unused dividend and split fetches, added functionality to handle stock splits. Adjusted database seeders and frontend calendar timezone for compatibility and consistency.

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -9,12 +9,12 @@ class CompanySeeder extends Seeder
 {
     public function run(): void
     {
-//        Company::create(['ticker' => 'AAPL', 'name' => 'Apple Inc.', 'country' => 'US', 'exchange' => 'NASDAQ', 'sector' => 'Technology', 'is_active' => true]);
+        Company::create(['ticker' => 'AAPL', 'name' => 'Apple Inc.', 'country' => 'US', 'exchange' => 'NASDAQ', 'sector' => 'Technology', 'is_active' => true]);
 //        Company::create(['ticker' => 'GOOGL', 'name' => 'Alphabet Inc.', 'country' => 'US', 'exchange' => 'NASDAQ', 'sector' => 'Technology', 'is_active' => true]);
 //        Company::create(['ticker' => 'MSFT', 'name' => 'Microsoft Corporation', 'country' => 'US', 'exchange' => 'NASDAQ', 'sector' => 'Technology', 'is_active' => true]);
 //
-//        Company::create(['ticker' => 'TSLA', 'name' => 'Tesla Inc.', 'country' => 'US', 'exchange' => 'NASDAQ', 'sector' => 'Consumer Cyclical', 'is_active' => true]);
+        Company::create(['ticker' => 'TSLA', 'name' => 'Tesla Inc.', 'country' => 'US', 'exchange' => 'NASDAQ', 'sector' => 'Consumer Cyclical', 'is_active' => true]);
 
-        Company::create(['ticker' => 'AMZN', 'name' => 'Amazon.com Inc.', 'country' => 'US', 'exchange' => 'NASDAQ', 'sector' => 'Consumer Cyclical', 'is_active' => true]);
+//        Company::create(['ticker' => 'AMZN', 'name' => 'Amazon.com Inc.', 'country' => 'US', 'exchange' => 'NASDAQ', 'sector' => 'Consumer Cyclical', 'is_active' => true]);
     }
 }
